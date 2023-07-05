799a4005b4afc4497abd2a138a040cd50361c01a
eCyclops <github@ecyclops.com>
2023-07-05 15:43:50 -0400

    Script renames

--- renameScripts.py
+++ renameScripts.py
@@ -9,9 +9,7 @@ Created on Wednesday July 05 2023
 #Import necessary python components
 
 import argparse
-import re
 import os
-import pathlib
 
 parser = argparse.ArgumentParser(description="Copy list of renamed scripts and name them back again. Look for stompable names.")
 # parser.add_argument("bookname", help="Name of the book we're extracting")
@@ -19,11 +17,13 @@ parser = argparse.ArgumentParser(description="Copy list of renamed scripts and n
 
 nowHere = os.getcwd()
 
-with open (nowHere+"macro_script_map.txt", "r") as namesList:
+with open (nowHere+"/macro_script_map.txt", "r") as namesList:
     for line in namesList:
         oldFile, newFile = line.split(" => ")
-        newPath = "library/mtscript/public/"+newFile+".mts"
+        newFile = newFile.replace("/", "-")
+        newPath = "library/mtscript/public/"+newFile.strip()+".mts"
         if(os.path.exists(newPath) and os.path.isfile(newPath)):
             print("File "+newPath+" already exists.")
         else:
+            print("Renaming "+oldFile+" to "+ newPath)
             os.rename("library/mtscript/public/"+oldFile,newPath,)
